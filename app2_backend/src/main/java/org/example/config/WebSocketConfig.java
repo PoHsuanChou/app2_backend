596f4921128f2dc6a41b7ca7aba491bd65903c15
chous <paul4017boy@gmail.com>
2025-02-22 11:24:36 +0800

    聊天室跟建立

--- app2_backend/src/main/java/org/example/config/WebSocketConfig.java
+++ app2_backend/src/main/java/org/example/config/WebSocketConfig.java
@@ -1,10 +1,16 @@
 package org.example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,9 +19,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:19006") // 改為您的 Expo 開發伺服器網址
-                .setAllowedOriginPatterns("*")  // 或使用這個來允許多個來源
-                .withSockJS();
+                .setAllowedOrigins(
+                        "http://localhost:19006",
+                        "http://localhost:8080",
+                        "http://10.0.2.2:8080"  // Android 模擬器
+                )
+                .setHandshakeHandler(new DefaultHandshakeHandler() {
+                    @Override
+                    protected Principal determineUser(ServerHttpRequest request,
+                                                      WebSocketHandler wsHandler,
+                                                      Map<String, Object> attributes) {
+                        // 打印握手詳情
+                        System.out.println("Handshake request: " + request.getHeaders());
+                        return super.determineUser(request, wsHandler, attributes);
+                    }
+                });
     }
 
     @Override
